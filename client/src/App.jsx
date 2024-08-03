a6fde1a5b3eb7c9b18be4eab9a439fcfab0bf440
Aman <amanverma0428@gmail.com>
2024-08-04 05:02:16 +0530

    added some routes and files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import Error404 from "./pages/Error404/Error404"
+import Home from "./pages/Home/Home"
+import Login from "./pages/Login/Login"
+import Register from "./pages/Register/Register"
+import Notification from "./pages/Notification/Notification"
+import Explore from "./pages/Explore/Explore"
+import Profile from "./pages/Profile/Profile"
+import UserDetails from "./pages/Profile/UserDetails/UserDetails"
+
 function App() {
 	return (
-		<>
-			<div className="underline text-3xl">Hello World</div>
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route path="*" element={<Error404 />} />
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/notification" element={<Notification />} />
+				<Route path="/explore" element={<Explore />} />
+				<Route path="profile/user/:userID" element={<Profile />} />
+				<Route path="profile/user/:userID/setting" element={<UserDetails />} />
+			</Routes>
+		</BrowserRouter>
 	)
 }
 
---
+++ client/src/pages/Profile/Profile.jsx
@@ -0,0 +1,9 @@
+import UserDetails from "./UserDetails/UserDetails"
+
+export default function Profile() {
+	return (
+		<div>
+			User Profile
+		</div>
+	)
+}
